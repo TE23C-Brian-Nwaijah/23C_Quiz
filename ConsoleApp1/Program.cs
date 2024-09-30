de3f6c7cd7d83c174ac2513846d8fc034eb91686
Brian Nwaijah <brian.nwaijah@elev.ga.ntig.se>
2024-09-30 18:34:30 +0200

    23C_quiz

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿//string name = "Brian";
+
+//string result = "6" + "4";
+
+//Console.WriteLine(result);
+
+using System.Net;
+using System.Text.Encodings.Web;
+
+int hp = 100;
+
+hp = hp - 10;
+hp -= 10;
+hp -= 10;
+Console.WriteLine("Välkommen till en random frågesport");
+
+//--------------------------------------------------------------------
+
+Console.WriteLine("vad heter den nuvarande presidenten för ryssland?");
+Console.WriteLine("a) Joe biden b) Putin c) Obama");
+string svar1 = Console.ReadLine();
+if (svar1 == "b")
+{
+hp++;
+Console.WriteLine("Correct Bra Jobbat");
+}
+else
+{
+    Console.WriteLine("INCORRECT DIN NOOB");
+}
+
+//nästa-----------------------------------------------
+Console.WriteLine("vem är det som skrivit denna frågesport?");
+Console.WriteLine("a) Albert b) Jag c) Brian");
+string svar2 = Console.ReadLine();
+if(svar1 == "c");
+{
+    hp++;
+Console.WriteLine("Grymt jobbat!,");
+}
+//nästa-----------------------------------------------
+Console.WriteLine("vem fightas mot skurkar på natten i läder");
+Console.WriteLine("a) Black noir b) Spiderman c) Batman");
+string svar3 = Console.ReadLine();
+if(svar1 == "c");
+{
+    hp++;
+Console.WriteLine("Bra svar!");
+}
